a22e0756e51c5c9e0c89ca86ecbf52c2118feb5a
aanton <aanton@gmail.com>
2021-12-05 12:08:26 +0100

    Day 3 challenge

---
+++ src/day03.js
@@ -0,0 +1,46 @@
+/*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
+
+/**
+ * @param {string} letter
+ * @return boolean
+ */
+export default function isValid(letter) {
+  let indexOpenMark = letter.indexOf('(')
+  let indexCloseMark = letter.indexOf(')')
+
+  while (true) {
+    if (indexOpenMark === -1 && indexCloseMark === -1) return true // No parenthesis is allowed
+    if (indexOpenMark === -1 || indexCloseMark === -1) return false // Invalid parenthesis pair
+
+    // At this point, both opene & close parenthesis are found
+
+    if (indexOpenMark > indexCloseMark) return false
+
+    const gift = letter.substring(indexOpenMark + 1, indexCloseMark)
+    if (gift === '') return false
+    if (gift.match(/[(){}[\]]/)) return false
+
+    indexOpenMark = letter.indexOf('(', indexCloseMark + 1)
+    indexCloseMark = letter.indexOf(')', indexCloseMark + 1)
+  }
+}
+
+/**
+ * @param {string} letter
+ * @return boolean
+ */
+export function isValidUsingRegex(letter) {
+  const gifts = letter.match(/\((.*?)(\)|$)/gm)
+
+  if (gifts === null) return true // No parenthesis is allowed
+
+  const invalidGift = gifts.find(gift => {
+    return (
+      gift.length === 2 ||
+      gift.slice(-1) !== ')' ||
+      gift.slice(1, -1).match(/[(){}[\]]/)
+    )
+  })
+
+  return invalidGift === undefined
+}

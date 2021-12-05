6d32de225518785bf7aa33da7dad9c51c80f025c
aanton <aanton@gmail.com>
2021-12-05 12:08:36 +0100

    Day 4 challenge

---
+++ src/day04.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} height
+ * @return string
+ */
+export default function createXmasTree(height) {
+  const data = []
+
+  for (let index = 1; index <= height; index++) {
+    const row =
+      '_'.repeat(height - index) +
+      '*'.repeat(index * 2 - 1) +
+      '_'.repeat(height - index)
+    data.push(row)
+  }
+
+  const trunk = '_'.repeat(height - 1) + '#' + '_'.repeat(height - 1)
+  data.push(trunk)
+  data.push(trunk)
+
+  return data.join('\n')
+}
---
+++ tests/day04.test.js
@@ -0,0 +1,28 @@
+import createXmasTree from '../src/day04'
+
+test('is a xmas tree with height of 3', () => {
+  // prettier-ignore
+  expect(createXmasTree(3)).toBe(
+    [
+      '__*__',
+      '_***_',
+      '*****',
+      '__#__',
+      '__#__'
+    ].join('\n')
+  );
+})
+
+test('is a xmas tree with height of 5', () => {
+  expect(createXmasTree(5)).toBe(
+    [
+      '____*____',
+      '___***___',
+      '__*****__',
+      '_*******_',
+      '*********',
+      '____#____',
+      '____#____',
+    ].join('\n')
+  )
+})
